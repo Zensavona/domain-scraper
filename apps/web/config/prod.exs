2e282c22db8605227cb326054f27b046f88997d9
Zen Savona <tedelex06@gmail.com>
2017-04-12 20:03:15 +1000

    config changes

--- apps/web/config/prod.exs
+++ apps/web/config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :web, Web.Endpoint,
   http: [port: 80],
-  url: [host: "test.prod", 80],
+  url: [host: "test.prod", port: 80],
   server: true,
   root: ".",
   cache_static_manifest: "priv/static/manifest.json"
