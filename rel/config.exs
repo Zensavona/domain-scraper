e1120265d4010c3f0189de35d3b8406ab9120475
Zen Savona <tedelex06@gmail.com>
2017-04-15 20:23:36 +1000

    0.1.6

--- rel/config.exs
+++ rel/config.exs
@@ -31,7 +31,7 @@ end
 # will be used by default
 
 release :domain_scraper do
-  set version: "0.1.5"
+  set version: "0.1.6"
   set applications: [
     finisher: :permanent,
     scheduler: :permanent,
