48acf727fa42558d09d52259a4095d660d553bd9
Zen Savona <tedelex06@gmail.com>
2017-04-16 16:30:59 +1000

    bump

--- rel/config.exs
+++ rel/config.exs
@@ -31,7 +31,7 @@ end
 # will be used by default
 
 release :domain_scraper do
-  set version: "0.1.8"
+  set version: "0.1.9"
   set applications: [
     finisher: :permanent,
     scheduler: :permanent,
