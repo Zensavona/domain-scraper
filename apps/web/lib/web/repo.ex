685df8d5814d3b6bb61d0c98e14064e5aadade9a
Zen Savona <tedelex06@gmail.com>
2017-05-03 13:33:27 +1000

    add migration function to Repo

--- apps/web/lib/web/repo.ex
+++ apps/web/lib/web/repo.ex
@@ -1,3 +1,9 @@
 defmodule Web.Repo do
   use Ecto.Repo, otp_app: :web
+
+  @migrations_dir Path.join([:code.priv_dir(:your_application), "repo", "migrations"])
+
+  def migrate do
+    Ecto.Migrator.run(__MODULE__, @migrations_dir, :up, [all: true])
+  end
 end
