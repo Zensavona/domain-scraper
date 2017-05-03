5af8b74653eca6318a1fa20ca1f25d083eaa94ad
Zen Savona <tedelex06@gmail.com>
2017-05-03 13:37:26 +1000

    fix app name

--- apps/web/lib/web/repo.ex
+++ apps/web/lib/web/repo.ex
@@ -1,7 +1,7 @@
 defmodule Web.Repo do
   use Ecto.Repo, otp_app: :web
 
-  @migrations_dir Path.join([:code.priv_dir(:your_application), "repo", "migrations"])
+  @migrations_dir Path.join([:code.priv_dir(:web), "repo", "migrations"])
 
   def migrate do
     Ecto.Migrator.run(__MODULE__, @migrations_dir, :up, [all: true])
